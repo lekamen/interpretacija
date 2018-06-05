02905fa15dc425b438cc9c941631790ebbcdbf48
Vedran Vinkovic <vedran.vinkovic2@gmail.com>
2018-06-05 17:57:06 +0200

    popravljena gramatika

--- DZ2_gramatika.py
+++ DZ2_gramatika.py
@@ -1,7 +1,8 @@
 # GRAMATIKA (puna)
+# identifier    -> [A-Za-z_][A-Za-z0-9_]*
 # broj          -> decimalni | heksadekadski
 # decimalni     -> 0 | [1-9][0-9]*
-# heksadekadski ->
+# heksadekadski -> 0[xX][0-9a-fA-F]+
 # string        -> "schar*"
 # char          -> 'cchar'
 # library       -> <lchar*>
@@ -10,7 +11,7 @@
 # nchar         -> (normalni znak osim ")
 # lchar         -> (normalni znak osim >)
 # escape        -> \n | \t | \v | \b | \r | \f | \a |
-#                  \\ | \' | \" 
+#                  \\ | \' | \"
 # separator     -> ( | ) | [ | ] | { | } | , | ;
 # unarni_op     -> ! | ~ | - | *
 # binarni_op    -> . | -> | * | / | % | + | - | << | >> |
